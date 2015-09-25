e6d78e71465fecdd44a01a2108336895d4c93014
Kolesnikov Alexander <alexander.kolesnikov@teknavo.com>
2015-09-25 14:20:14 +0300

    admin pages

---
+++ app/controllers/admin/orders_controller.rb
@@ -0,0 +1,53 @@
+class Admin::OrdersController < ApplicationController
+  before_action :set_order, only: [:show]
+
+  # GET /orders
+  # GET /orders.json
+  def index
+    @orders = Order.all
+  end
+
+  # GET /orders/1
+  # GET /orders/1.json
+  def show
+  end
+
+  # GET /orders/new
+  def new
+    @order = Order.new
+  end
+
+  # POST /orders
+  # POST /orders.json
+  def create
+    @order = Order.new(order_params)
+
+    respond_to do |format|
+      if @order.save
+        format.html { redirect_to @order, notice: 'Order was successfully created.' }
+        format.json { render :show, status: :created, location: @order }
+
+        @basket = ActiveSupport::JSON.decode(cookies["basket"])
+        p @basket
+        @basket.each do |order_position|
+          @order_detail = OrderDetail.create!(order: @order, product_id: order_position[0], qty: order_position[1])
+          p @order_detail
+        end
+      else
+        format.html { render :new }
+        format.json { render json: @order.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_order
+      @order = Order.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def order_params
+      params.require(:order).permit(:email, :total)
+    end
+end
---
+++ app/views/admin/products/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @product, :id, :title, :description, :price, :stock_level, :image, :created_at, :updated_at
